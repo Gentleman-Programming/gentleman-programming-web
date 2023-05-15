34b1ec2baa36a9579cc24fc4a6c7c9563f1f89a9
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-05-15 02:30:53 -0500

    FIX: ARREGLOS DE COLORES Y CSS Y GRID

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { ReactNode } from 'react';
 
+import style from '@page/styles/layout.module.css';
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={style.app__grid}>
       <head />
       <body>{children}</body>
     </html>
