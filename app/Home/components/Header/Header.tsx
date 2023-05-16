bf0cafa9b95021133ed8a2f1927a19157f30aa7e
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-05-15 22:51:04 -0500

    fix: act arch home

--- app/Home/components/Header/Header.tsx
+++ app/Home/components/Header/Header.tsx
@@ -4,8 +4,8 @@ import style from './header.module.css';
 
 export const Header: FC = () => {
   return (
-    <div className={style.header__container}>
+    <header className={style.header__container}>
       <h1>Web de Gentleman Programming</h1>
-    </div>
+    </header>
   );
 };
