46209af79a558c0114b88cd43f8f6e1021b45846
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-05-17 23:34:08 -0500

    fix: act de navbar con icons

--- app/components/Navbar/components/NavList/NavList.tsx
+++ app/components/Navbar/components/NavList/NavList.tsx
@@ -1,4 +1,4 @@
-import { ServerButton } from '@app/components';
+import { ServerButton } from 'app/components';
 
 import style from './navList.module.css';
 
@@ -11,6 +11,7 @@ export function NavList() {
         mentions={3}
         isHome={true}
         isLive={true}
+        img="GP"
       />
       <div className={style.navList__separador}></div>
       <ServerButton
@@ -18,14 +19,16 @@ export function NavList() {
         selected={true}
         link="https://google.com"
         mentions={3}
+        img="TW"
       />
-      <ServerButton description="Hola" link="https://google.com" />
-      <ServerButton description="Hola" link="https://google.com" />
-      <ServerButton description="Hola" link="https://google.com" />
-      <ServerButton description="Hola" link="https://google.com" />
-      <ServerButton description="Hola" link="https://google.com" />
-      <ServerButton description="Hola" link="https://google.com" />
-      <ServerButton description="Hola" link="https://google.com" />
+      <ServerButton description="Hola" link="https://google.com" img="DS" />
+      <ServerButton description="Hola" link="https://google.com" img="YT" />
+      <ServerButton description="Hola" link="https://google.com" img="TT" />
+      <ServerButton description="Hola" link="https://google.com" img="LK" />
+      <ServerButton description="Hola" link="https://google.com" img="GH" />
+      <ServerButton description="Hola" link="https://google.com" img="IN" />
+      <ServerButton description="Hola" link="https://google.com" img="SP" />
+      <ServerButton description="Hola" link="https://google.com" img="TK" />
     </div>
   );
 }
