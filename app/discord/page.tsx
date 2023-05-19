e5fd47dbd7c0265401633ebcb66e6b09a4451ef4
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-05-19 01:04:53 -0500

    FIX: ACT ARCH

--- app/discord/page.tsx
+++ app/discord/page.tsx
@@ -2,6 +2,9 @@ import '@app/styles/global.css';
 
 import { Container } from './components';
 import style from './page.module.css';
+export const metadata = {
+  title: 'GP -> DISCORD',
+};
 export default function Discord() {
   return (
     <div className={style.discord__theme}>
