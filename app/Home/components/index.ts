0559a59a27cf8d822a4d90a56878a00388950a86
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-05-23 21:08:17 -0500

    fix: act arch

--- app/Home/components/index.ts
+++ app/Home/components/index.ts
@@ -1,7 +1,8 @@
 export * from './Body';
 export { default as Controller } from './Footer/Controller';
 export { default as Footer } from './Footer/Footer';
+export { default as FooterCard } from './Footer/FooterCard';
 export * from './Footer/Icons';
 export * from './Header';
+export { default as AlanCard } from './PreFooter/AlanCard';
 export { default as Prefooter } from './PreFooter/Prefooter';
-export { default as FooterCard } from './Footer/FooterCard';
--- app/Home/index.ts
+++ app/Home/index.ts
@@ -1,9 +1,11 @@
-export * from './Home';
+export * from './components/Body';
 export { default as Controller } from './components/Footer/Controller';
 export { default as Footer } from './components/Footer/Footer';
+export { default as FooterCard } from './components/Footer/FooterCard';
 export * from './components/Footer/Icons';
 export * from './components/Header';
+export { default as AlanCard } from './components/PreFooter/AlanCard';
 export { default as Prefooter } from './components/PreFooter/Prefooter';
-export { default as FooterCard } from './components/Footer/FooterCard';
 export * from './components';
-export * from './index';
+export { default as layout } from './layout';
+export * from './Home';
