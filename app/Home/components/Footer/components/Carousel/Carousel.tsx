86c4b02ee396353bf78541140aedaf0e3e65af55
Angel Scutari <antonioscutari2@hotmail.com>
2023-06-02 02:15:37 -0300

    fix feature: carrousel is working

--- app/Home/components/Footer/components/Carousel/Carousel.tsx
+++ app/Home/components/Footer/components/Carousel/Carousel.tsx
@@ -7,59 +7,65 @@ type Props = {
   team: Team;
 };
 export const Carousel: FC<Props> = ({ team }) => {
+  const renderMember = (member: Member) => {
+    const { visibility, name, index } = member;
+
+    if (!visibility) {
+      return null;
+    }
+    return (
+      <>
+        <input
+          type={'radio'}
+          name={'radio-buttons'}
+          id={`${index}`}
+          checked={index === 1}
+        />
+        <li className={styles.carouselSlideContainer} key={'slide' + name}>
+          <div className={styles.carouselSlideCard}>
+            <FooterCard key={'card' + name} member={member} />
+          </div>
+          {/*CONTROLES */}
+          <div className={styles.carouselControls}>
+            {index > 1 && (
+              <label
+                htmlFor={`${index - 1}`}
+                className={styles.carouselPrevSlide}
+              >
+                <span>&lsaquo;</span>
+              </label>
+            )}
+            {index < team.length - 1 && (
+              <label
+                htmlFor={`${index + 1}`}
+                className={styles.carouselNextSlide}
+              >
+                <span>&rsaquo;</span>
+              </label>
+            )}
+          </div>
+          {/*CONTROLES */}
+        </li>
+      </>
+    );
+  };
   return (
     <section className={styles.carousel}>
       <ul className={styles.carouselSlides}>
-        {team.map(
-          (member: Member) =>
-            member.visibility && (
-              <>
-                <input
-                  type={'radio'}
-                  name={'radio-buttons'}
-                  id={`${member.index}`}
-                  checked
+        {team.map(renderMember)}
+        <div className={styles.carouselDots}>
+          {team.map(
+            (member) =>
+              member.visibility && (
+                <label
+                  key={member.index}
+                  htmlFor={`${member.index}`}
+                  className={styles.carouselDot}
+                  id={'dot-' + member.index}
                 />
-                <li
-                  className={styles.carouselSlideContainer}
-                  key={'slide' + member.name}
-                >
-                  <div className={styles.carouselSlideCard}>
-                    <FooterCard key={'card' + member.name} member={member} />
-                  </div>
-                  {/*CONTROLES */}
-                  {member.visibility && (
-                    <div className={styles.carouselControls}>
-                      <label
-                        htmlFor={`${member.index - 1}`}
-                        className={styles.carouselPrevSlide}
-                      >
-                        <span>&lsaquo;</span>
-                      </label>
-                      <label
-                        htmlFor={`${member.index + 1}`}
-                        className={styles.carouselNextSlide}
-                      >
-                        <span>&rsaquo;</span>
-                      </label>
-                    </div>
-                  )}
-                  {/*CONTROLES */}
-                </li>
-                {/*puntos*/}
-                <div className={styles.carouselDots}>
-                  {member.visibility && (
-                    <label
-                      htmlFor={`${member.index}`}
-                      className={styles.carouselDot}
-                      id={'dot-' + member.index}
-                    />
-                  )}
-                </div>
-                {/*puntos*/}
-              </>
-            )
-        )}
+              )
+          )}
+        </div>
       </ul>
     </section>
   );
