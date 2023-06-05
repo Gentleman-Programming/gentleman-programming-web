2a3c088b3208bfe2a8ef2a97b0d22d8f0a3a1ef7
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-06-05 02:50:52 -0500

    fix: Updating the documentation in the files.

--- app/components/TutiFooterFinal/TutiFooterFinal.tsx
+++ app/components/TutiFooterFinal/TutiFooterFinal.tsx
@@ -1,14 +1,27 @@
+/* `import { FC } from 'react'. */
 import { FC } from 'react';
 
+/* `import styles from './styles/TutiFooterFinal.module.css'. */
 import styles from './styles/TutiFooterFinal.module.css';
-import { TutiHr } from '../TutiHr';
-
+/* his component is then used in the `TutiFooterFinal` component to render a horizontal
+line. */
+import { TutiHr /* is TutiHr the component */ } from '../TutiHr';
+/**
+ * Renders a TutiFooterFinal component.
+ * @returns {JSX.Element} The rendered component.
+ */
 export const TutiFooterFinal: FC = () => {
+  // Render a container div that wraps the rest of the component
   return (
+    /* Rendering a container `div` element with a class name of `tutiFooterFinal__container`. */
     <div className={styles.tutiFooterFinal__container}>
+      {/* Render a horizontal rule */}
       <TutiHr />
+      {/* Render a section that contains the last paragraph and legend */}
       <section className={styles.tutiFooterFinal__lastSection}>
+        {/* Render a paragraph thanking the community */}
         <p>Made with 💜 by the community</p>
+        {/* Render a legend with company information */}
         <legend>
           Gentleman Programming⟨™⟩ 2023 // Casi todos los derechos reservados{' '}
         </legend>
