b7ab11c1d082177074b3b76b2a617f96f1dae45c
Angel Scutari <antonioscutari2@hotmail.com>
2023-06-06 22:15:21 -0300

    mod controller: functionality.

--- app/Home/components/Footer/components/Controller/Controller.tsx
+++ app/Home/components/Footer/components/Controller/Controller.tsx
@@ -18,35 +18,68 @@ export const Controller = () => {
       <header>Enlaces al exterior:</header>
       <section className={controlStyles.footer__controllerUp}>
         <button className={controlStyles.footer__controllerCard1}>
-          <InstagramIcon />
+          <a
+            href="https://www.instagram.com/gentlemanprogramming"
+            target="__blank"
+          >
+            <InstagramIcon />
+          </a>
         </button>
         <button className={controlStyles.footer__controllerCard2}>
-          <GithubIcon />
+          <a href="https://github.com/Gentleman-Programming" target="__blank">
+            <GithubIcon />
+          </a>
         </button>
         <button className={controlStyles.footer__controllerCard7}>
-          <TikTokIcon />
+          <a
+            href="https://www.tiktok.com/@gentlemanprogramming"
+            target="__blank"
+          >
+            <TikTokIcon />
+          </a>
         </button>
         <button className={controlStyles.footer__controllerCard3}>
-          <DiscordIcon />
+          <a href="https://discord.com/invite/KEavKkDc5Y" target="__blank">
+            <DiscordIcon />
+          </a>
         </button>
       </section>
       <section className={controlStyles.footer__controllerMiddle}>
         <button className={controlStyles.footer__controllerCardMid}>
-          <YouTubeIcon />
+          <a
+            href="https://www.youtube.com/c/GentlemanProgramming"
+            target="__blank"
+          >
+            <YouTubeIcon />
+          </a>
         </button>
       </section>
       <section className={controlStyles.footer__controllerDown}>
         <button className={controlStyles.footer__controllerCard4}>
-          <TwitterIcon />
+          <a href="https://twitter.com/G_Programming" target="__blank">
+            <TwitterIcon />
+          </a>
         </button>
         <button className={controlStyles.footer__controllerCard5}>
-          <SpotifyIcon />
+          <a
+            href="https://open.spotify.com/show/4AhopzZexw2wOMJyIcBWlx?si=3e3f8c8fd5844b66"
+            target="__blank"
+          >
+            <SpotifyIcon />
+          </a>
         </button>
         <button className={controlStyles.footer__controllerCard8}>
-          <LinkedInIcon />
+          <a href="https://www.linkedin.com/in/alanbuscaglia/" target="__blank">
+            <LinkedInIcon />
+          </a>
         </button>
         <button className={controlStyles.footer__controllerCard6}>
-          <TwitchIcon />
+          <a
+            href="https://www.twitch.tv/gentleman_programming"
+            target="__blank"
+          >
+            <TwitchIcon />
+          </a>
         </button>
       </section>
     </section>
