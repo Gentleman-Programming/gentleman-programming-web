c7b2790b8c7fe52f12a943ee324bce34b2f12b40
Angel Scutari <antonioscutari2@hotmail.com>
2023-06-10 23:30:26 -0300

    add style and barrels: font and banner

--- app/youtube/components/Home/HomeYoutube.tsx
+++ app/youtube/components/Home/HomeYoutube.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 
 import data from '../../Data/response.json';
+import styles from '../../styles/youtube.module.css';
 
 const HomeYoutube = () => {
   const { items } = data;
-  const channelTitle = items[0].snippet.channelTitle;
+  const channelTitle = items[0].brandingSettings?.channel.title;
+  const channelBanner = items[0].brandingSettings?.image.bannerExternalUrl;
   return (
     <main>
-      <header>
-        {/* <img src="youtube_logo.png" alt="YouTube Logo"/> */}
+      <header className={styles.youtube__navBarSection}>
         <input type="text" placeholder="Search" />
-        <button type="submit">Search</button>
+        <button type="submit">Lupa</button>
+        {/* call the "chrome.identity.getProfileUserInfo" function to retrieve the user's basic profile information. Note that the will need to grant your extension permission to access their profile information. To request this permission, you can call the "chrome.identity.getAuthToken" function, which will prompt the user to sign in to their Google account and grant your extension access to their basic profile information.  */}
         {/* <img src="user_avatar.png" alt="User Avatar"/> */}
       </header>
-      <div className="channel-banner">
-        {/* <img src="channel_banner.png" alt="Channel Banner" /> */}
+      <div className={styles.youtube__banner}>
+        <img src={channelBanner} alt="Gentleman Banner" />
       </div>
       <div className="channel-details">
         <h1>{channelTitle}</h1>
