0b44080e1d5d0fc2ed3a1ee2044f6885d28c60c3
Angel Scutari <antonioscutari2@hotmail.com>
2023-06-24 01:12:30 -0300

    updated barrel

--- app/youtube/components/index.ts
+++ app/youtube/components/index.ts
@@ -1,1 +1,8 @@
+export * from './ChannelDetails/ChannelDetails';
+export * from './ChannelTabs/TabsBar';
+export * from './HeaderSearchbar/HeaderSearchbar';
 export { default as HomeYoutube } from './Home/HomeYoutube';
+export * from './Home/SearchIcon';
+export * from './Playlists/YTPlayslists';
+export * from './VideoCard/YTVideoCard';
+export * from './index';
