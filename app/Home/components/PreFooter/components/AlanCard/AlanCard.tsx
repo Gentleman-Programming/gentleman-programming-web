8e91aeb832b5b50a0b52b0befd99c33b4e1dbc6a
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-06-30 01:36:57 -0500

    feat: :zap: tuti zoom
    tuti zoom
    
    [DcDevRep/issue70]

--- app/Home/components/PreFooter/components/AlanCard/AlanCard.tsx
+++ app/Home/components/PreFooter/components/AlanCard/AlanCard.tsx
@@ -1,15 +1,18 @@
-import Alan from '@app/assets/img/AlanBuscaglia.png';
-import Image from 'next/image';
+//import Alan from '@app/assets/img/AlanBuscaglia.png';
+import { TutiImgZoom } from '@app/common';
+//import Image from 'next/image';
 
 import styles from './styles/alanCard.module.css';
+
 export const AlanCard = () => {
   return (
     <article className={styles.footer__alanCard}>
-      <Image
+      <TutiImgZoom />
+      {/* <Image
         className={styles.footer__alanImg}
         src={Alan}
         alt="foto de Alan Buscaglia"
-      />
+      /> */}
       <section className={styles.footer__textContainer}>
         <header className={styles.footer__alanTitle}>Alan Buscaglia</header>
         <p>Ingeniero y arquitecto de Software Front-end</p>
