f13e4b329f43765444209dc14df4d75d6fa14cf1
Angel Scutari <antonioscutari2@hotmail.com>
2023-07-08 23:23:48 -0300

    new add: youtube pages => Shorts & Videos

---
+++ app/youtube/components/ChannelSections/Shorts/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Shorts() {
+  return (
+    <main>
+      <button>Más recientes</button>
+      <button>Popular</button>
+      <section>Shorts</section>
+    </main>
+  );
+}
+
+export default Shorts;
