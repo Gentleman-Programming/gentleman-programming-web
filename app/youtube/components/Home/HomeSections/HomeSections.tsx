3c26b49583b306ec23f788a11c8577c7d8f058c9
Angel Scutari <antonioscutari2@hotmail.com>
2023-07-24 20:54:41 -0300

    mod style: added bars(hr) && names to sections

--- app/youtube/components/Home/HomeSections/HomeSections.tsx
+++ app/youtube/components/Home/HomeSections/HomeSections.tsx
@@ -10,18 +10,53 @@ import { SinglePlaylist } from '../../ChannelSections/Playlists/Playlists/YTSing
 export const ChannelSections = () => {
   return (
     <section className={style.youtubeChannelSectionsContainer}>
-      {sectionData.items[1].snippet.position === 1 ? null : (
-        <hr className={hr.youtube__divider} />
-      )}
       {sectionData.items.map(
         (section) =>
           dataSinglePLs[section.snippet.position] && (
-            <SinglePlaylist
+            <section
               key={dataSinglePLs[section.snippet.position].index}
-              section={dataSinglePLs[section.snippet.position]}
-            />
+              className={`home-section-${
+                dataSinglePLs[section.snippet.position].index
+              }`}
+            >
+              {dataSinglePLs[section.snippet.position].index === 1 ? null : (
+                <hr className={hr.youtube__divider} />
+              )}
+              <p>{nameGiver(dataSinglePLs[section.snippet.position].index)}</p>
+              <SinglePlaylist
+                key={dataSinglePLs[section.snippet.position].index}
+                section={dataSinglePLs[section.snippet.position]}
+              />
+            </section>
           )
       )}
     </section>
   );
 };
+function nameGiver(index: number) {
+  //This is a function I made to name the sections in the HOME
+  switch (index) {
+    case 1:
+      return 'LO MEJORCITO DEL CANAL 😱';
+    case 2:
+      return 'Videos';
+    case 3:
+      return '🆓Cursos GRATIS';
+    case 4:
+      return 'Shorts';
+    case 5:
+      return '🧑‍💻 Curso de Javascript con de tuti';
+    case 6:
+      return '⚛️React : desde bases hasta conceptos avanzados🚀';
+    case 7:
+      return '🅰Angular: de 0 a EXPERTO🚀';
+    case 8:
+      return '👷🏻‍♂️Clean Architecture🧼';
+    case 9:
+      return 'Hexagonal Architecture🍕';
+    case 10:
+      return '🎩 Gentleman Mentoring 🥋';
+    default:
+      return null;
+  }
+}
